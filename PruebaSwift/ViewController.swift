d0de53bfda52311a642b5a3b4468722ad8f6fe86
Eduardo Espinoza kramsky <ekramsky@inflexionsoftware.com>
2016-02-12 00:25:26 -0600

    Initial Commit

---
+++ PruebaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaSwift
+//
+//  Created by Eduardo Espinoza kramsky on 12/02/16.
+//  Copyright © 2016 Nopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
